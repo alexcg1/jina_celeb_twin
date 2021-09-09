623d804c2f93d9275de557bcd74ebb22795b8b8b
Alex C-G <alexcg@outlook.com>
2021-09-09 10:09:43 +0200

    feat: more endpoints/datadirs

--- frontend-streamlit/config.py
+++ frontend-streamlit/config.py
@@ -1,6 +1,9 @@
 # Absolute path prefix. This lets us copy workspaces between machines. So we can index on a fast machine and query from a slow one
-data_dir_root = "/mnt/data/work/repos/personal/jina_celeb_twin/backend/data"
+# data_dir_root = "/mnt/data/work/repos/personal/jina_celeb_twin/backend/" # local
+data_dir_root = "/home/ubuntu/alexcg/celeb_twin/backend/" # aws
+
 image_endpoint = "http://0.0.0.0:54321/search"
+image_endpoint = "http://examples.jina.ai:54321/search"
 
 top_k = 10
 
